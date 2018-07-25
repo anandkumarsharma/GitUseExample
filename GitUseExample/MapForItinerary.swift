92c618a9d7f7a75950baddbdf4702e559ccb3471
Ajay Pandey <ajay@ajays-macbook-pro.local>
2018-07-25 17:03:11 +0530

    This is second Commit after add Map View

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Ajay Pandey on 25/07/18.
+//  Copyright © 2018 Ajay Pandey. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
