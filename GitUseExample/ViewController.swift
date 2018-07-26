e4933fc7cd2cf584b09c53ab19e4f2bcda4a8501
AncileMac2 <ancilemac2@ancilemac2s-mac-mini.local>
2018-07-26 11:52:34 +0530

    tag testing by krishna

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("djsfadjsfsdfds");
+        print("djsfadjsfsdfds");
         // Do any additional setup after loading the view, typically from a nib.
         
         print("This is a Git tutorial")
